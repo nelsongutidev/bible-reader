6d7a9d4e796fb1cf95bf847a0c555ea6cbd653f6
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2025-02-13 22:52:16 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, resource } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'bible-reader';
+
+  todosResource = resource({
+    loader: () => {
+      return fetch(
+        `https://bible.helloao.org/api/available_translations.json`
+      ).then((res) => res.json() as Promise<any[]>);
+    },
+  });
 }
